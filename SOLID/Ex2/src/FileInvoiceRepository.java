59c3f247a208cef6b4468db1b8ebeca8afc9cb41
sathwikperla <perlasathwik65@gmail.com>
2026-03-02 11:26:05 +0530

    Final cleaned Ex1

--- SOLID/Ex2/src/FileInvoiceRepository.java
+++ SOLID/Ex2/src/FileInvoiceRepository.java
@@ -5,10 +5,12 @@ public class FileInvoiceRepository implements InvoiceRepository {
         this.store = store;
     }
 
+    @Override
     public void save(String id, String content) {
         store.save(id, content);
     }
 
+    @Override
     public int countLines(String id) {
         return store.countLines(id);
     }
